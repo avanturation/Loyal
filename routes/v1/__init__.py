701bc8728f5f4b272a99669dd91a34135fc1272c
fxrcha <truetype24@gmail.com>
2021-07-06 20:34:41 +0900

    chore: fixed imports

--- routes/v1/__init__.py
+++ routes/v1/__init__.py
@@ -3,7 +3,7 @@ from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
 from slowapi import Limiter
 from slowapi.util import get_remote_address
-from ...utils.cache import cache
+from utils.cache import cache
 
 v1_limiter = Limiter(key_func=get_remote_address)
 v1_router = InferringRouter()
